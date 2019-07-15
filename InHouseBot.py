0dd8852c726a65522eadf5a356ecead3f46ca10a
Chris Evans <chris.evans93@gmail.com>
2019-07-15 17:24:00 -0400

    Fix formatting, remove windows carridge returns

--- InHouseBot.py
+++ InHouseBot.py
@@ -3,10 +3,10 @@ from discord.ext import commands
 import random
 import asyncio
 
-prefix = '!'  # change this to whatever prefix you'd like
+prefix = "!"  # change this to whatever prefix you'd like
 bot = commands.Bot(command_prefix=prefix)
 # add roles that can use some commands
-approved_roles = ['Admin', 'Bot', 'Mod']
+approved_roles = ["Admin", "Bot", "Mod"]
 
 
 def is_approved():
@@ -14,6 +14,7 @@ def is_approved():
         author = ctx.message.author
         if any(role.name in approved_roles for role in author.roles):
             return True
+
     return commands.check(predicate)
 
 
@@ -28,9 +29,10 @@ class Queue(commands.Cog):
         self.queue = []
         self.qtoggle = True
 
-    @commands.command(pass_context=True, name='commands')
+    @commands.command(pass_context=True, name="commands")
     async def _commands(self, ctx):
-        await ctx.send('**!add** = Add yourself to the queue. \
+        await ctx.send(
+            "**!add** = Add yourself to the queue. \
                         \n**!remove** = Remove yourself from the queue.\
                         \n**!queue** = See the current queue.\
                         \n**!position** = See your current position in the queue.\
@@ -47,111 +49,118 @@ class Queue(commands.Cog):
                         \n**!fuckchong** = Honestly, fuck him.\
                         \n**!grime** = Important slice of in-house history...\
                         \n**!ass** = It is the truth.\
-                        \n**!morg** = Will always be funny.')
+                        \n**!morg** = Will always be funny."
+        )
 
     @commands.command(pass_context=True)
     async def add(self, ctx):
-        ''': Add yourself to the queue!'''
+        """: Add yourself to the queue!"""
         author = ctx.message.author
         if self.qtoggle:
             if author.id not in self.queue:
                 self.queue.append(author.id)
-                await ctx.send('you have been added to the queue.')
+                await ctx.send("you have been added to the queue.")
             else:
-                await ctx.send('you are already in the queue!')
+                await ctx.send("you are already in the queue!")
         else:
-            await ctx.send('The queue is closed.')
+            await ctx.send("The queue is closed.")
 
     @commands.command(pass_context=True)
     async def remove(self, ctx):
-        ''': Remove yourself from the queue'''
+        """: Remove yourself from the queue"""
         author = ctx.message.author
         if author.id in self.queue:
             self.queue.remove(author.id)
-            await ctx.send('you have been removed from the queue.')
+            await ctx.send("you have been removed from the queue.")
         else:
-            await ctx.send('you were not in the queue.')
+            await ctx.send("you were not in the queue.")
 
-    @commands.command(name='queue', pass_context=True)
+    @commands.command(name="queue", pass_context=True)
     async def _queue(self, ctx):
-        ''': See who's up next!'''
+        """: See who's up next!"""
         server = ctx.guild
-        message = ''
+        message = ""
         for place, member_id in enumerate(self.queue):
             member = discord.utils.get(server.members, id=member_id)
-            message += f'**#{place+1}** : {member.name}\n'
-        if message != '':
+            message += f"**#{place+1}** : {member.name}\n"
+        if message != "":
             await ctx.send(message)
         else:
-            await ctx.send('Queue is empty')
+            await ctx.send("Queue is empty")
 
     @commands.command(pass_context=True)
     async def position(self, ctx):
-        ''': Check your position in the queue'''
+        """: Check your position in the queue"""
         author = ctx.message.author
         if author.id in self.queue:
-            _position = self.queue.index(author.id)+1
-            await ctx.send(f'you are **#{_position}** in the queue.')
+            _position = self.queue.index(author.id) + 1
+            await ctx.send(f"you are **#{_position}** in the queue.")
         else:
-            await ctx.send(f'you are not in the queue, please use {prefix}add to add yourself to the queue.')
+            await ctx.send(
+                f"you are not in the queue, please use {prefix}add to add yourself to the queue."
+            )
 
-    @commands.command(pass_context=True, name='next')
+    @commands.command(pass_context=True, name="next")
     async def _next(self, ctx, num=1):
-        ''': Call the next member in the queue'''
+        """: Call the next member in the queue"""
         if len(self.queue) > 0:
             for x in range(num):
                 member = discord.utils.get(ctx.guild.members, id=self.queue[0])
-                await ctx.send(f'You are up **{member.mention}**! Have fun!')
+                await ctx.send(f"You are up **{member.mention}**! Have fun!")
                 self.queue.remove(self.queue[0])
 
     @is_approved()
     @commands.command(pass_context=True)
     async def clear(self, ctx):
-        ''': Clears the queue'''
+        """: Clears the queue"""
         self.queue = []
-        await ctx.send('Queue has been cleared')
+        await ctx.send("Queue has been cleared")
 
     @is_approved()
     @commands.command(pass_context=True)
     async def toggle(self, ctx):
-        ''': Toggles the queue'''
+        """: Toggles the queue"""
         self.qtoggle = not self.qtoggle
         if self.qtoggle:
-            state = 'OPEN'
+            state = "OPEN"
         else:
-            state = 'CLOSED'
-        await ctx.send(f'Queue is now {state}')
+            state = "CLOSED"
+        await ctx.send(f"Queue is now {state}")
 
     @commands.command(pass_context=True)
     async def fuckchong(self, ctx):
-        msg = await ctx.send(f'FUCK CHONG')
-        await msg.add_reaction('\U0001F1EB')
-        await msg.add_reaction('\U0001F1FA')
-        await msg.add_reaction('\U0001F1E8')
-        await msg.add_reaction('\U0001F1F0')
-        await msg.add_reaction('\U000021AA')
-        await msg.add_reaction('\U0001F1ED')
-        await msg.add_reaction('\U0001F1F4')
-        await msg.add_reaction('\U0001F1F3')
-        await msg.add_reaction('\U0001F1EC')
-        emoji = ':FuckChong:'
+        msg = await ctx.send(f"FUCK CHONG")
+        await msg.add_reaction("\U0001F1EB")
+        await msg.add_reaction("\U0001F1FA")
+        await msg.add_reaction("\U0001F1E8")
+        await msg.add_reaction("\U0001F1F0")
+        await msg.add_reaction("\U000021AA")
+        await msg.add_reaction("\U0001F1ED")
+        await msg.add_reaction("\U0001F1F4")
+        await msg.add_reaction("\U0001F1F3")
+        await msg.add_reaction("\U0001F1EC")
+        emoji = ":FuckChong:"
         await msg.add_reaction(emoji)
 
     @commands.command(pass_context=True)
     async def ass(self, ctx):
-        await ctx.send(f'CHONG IS AN ASS EATER')
+        await ctx.send(f"CHONG IS AN ASS EATER")
 
     @commands.command(pass_context=True)
     async def grime(self, ctx):
-        await ctx.send(f'On May 6th, 2019, Chong invited an ex-LCS player to the server...')
+        await ctx.send(
+            f"On May 6th, 2019, Chong invited an ex-LCS player to the server..."
+        )
 
     @commands.command(pass_context=True)
     async def morg(self, ctx):
-        await ctx.send(f'https://media.discordapp.net/attachments/569646728224178184/598615204288397501/unknown.png?width=1250&height=676')
+        await ctx.send(
+            f"https://media.discordapp.net/attachments/569646728224178184/598615204288397501/unknown.png?width=1250&height=676"
+        )
 
     @commands.command(pass_context=True)
     async def flip(self, ctx):
-        flip = ['Heads', 'Tails']
+        flip = ["Heads", "Tails"]
         ranflip = random.choice(flip)
         await ctx.send(ranflip)
 
@@ -162,52 +171,56 @@ class Queue(commands.Cog):
     @commands.command()
     async def roll(self, ctx, dice: str):
         try:
-            rolls, limit = map(int, dice.split('d'))
+            rolls, limit = map(int, dice.split("d"))
         except Exception:
-            await ctx.send('AdX format only(A=number of die, X=number of faces)')
+            await ctx.send("AdX format only(A=number of die, X=number of faces)")
             return
 
-        result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+        result = ", ".join(str(random.randint(1, limit)) for r in range(rolls))
         await ctx.send(result)
 
     @commands.command(pass_context=True)
     async def captains(self, ctx):
-        ''': Randomizes captains list from General Voice channel'''
-        members = discord.utils.get(ctx.guild.channels, name='General', type=discord.ChannelType.voice).members
+        """: Randomizes captains list from General Voice channel"""
+        members = discord.utils.get(
+            ctx.guild.channels, name="General", type=discord.ChannelType.voice
+        ).members
         random.shuffle(members)
-        message = ''
+        message = ""
         for place, member in enumerate(members):
             name = member.nick if member.nick else member.name
-            message += f'**#{place+1}** : {name}\n'
+            message += f"**#{place+1}** : {name}\n"
         if not message:
-            message = 'No voice lobby for captains draft'
+            message = "No voice lobby for captains draft"
         await ctx.send(message)
 
     @commands.command(pass_context=True)
     async def lulcaptains(self, ctx):
-        ''': Randomizes captains list from General Voice channel'''
-        members = discord.utils.get(ctx.guild.channels, name='General', type=discord.ChannelType.voice).members
+        """: Randomizes captains list from General Voice channel"""
+        members = discord.utils.get(
+            ctx.guild.channels, name="General", type=discord.ChannelType.voice
+        ).members
         random.shuffle(members)
-        message = ''
+        message = ""
         alphabet = "abcdefghijklmnopqrstuvwxyz"
         for place, member in enumerate(members):
             name = member.nick if member.nick else member.name
-            danny_name = ''
+            danny_name = ""
             for c in name:
-                if(random.randrange(2)):
+                if random.randrange(2):
                     danny_name += c + alphabet[random.randrange(len(alphabet))]
                 else:
                     danny_name += c
-            message += f'**#{place+1}** : {danny_name}\n'
+            message += f"**#{place+1}** : {danny_name}\n"
         if not message:
-            message = 'No voice lobby for captains draft'
+            message = "No voice lobby for captains draft"
         await ctx.send(message)
 
     @commands.command(pass_context=True)
     async def leggo(self, ctx):
-        '''Tries to get a game ready'''
+        """Tries to get a game ready"""
 
-        _message = await ctx.send('Who gaming, react @here')
+        _message = await ctx.send("Who gaming, react @here")
         react_count = 0
         reactions = []
         # open our queue for spill
@@ -218,14 +231,16 @@ class Queue(commands.Cog):
         while react_count < 10:
             # Let's not hog bot thread
             await asyncio.sleep(3)
-            seconds_elapsed +=3
+            seconds_elapsed += 3
             if seconds_elapsed > 5400:
-                await ctx.send('Timing out our gaming call, try again later :(')
+                await ctx.send("Timing out our gaming call, try again later :(")
                 return
             # Must refetch message otherwise coroutine never revaluates msg cache
             message = await ctx.fetch_message(_message.id)
             reactions = message.reactions
-            react_count = sum(map(lambda x: x.count, reactions))  # Can't do length of list since a reaction can have multiple reacts
+            react_count = sum(
+                map(lambda x: x.count, reactions)
+            )  # Can't do length of list since a reaction can have multiple reacts
 
         message = "Game ready, let's fuckin goooooo: \n\n"
         users = []
@@ -235,12 +250,12 @@ class Queue(commands.Cog):
 
         for place, user in enumerate(users):
             name = user.nick if user.nick else user.name
-            if place+1 > 10:
+            if place + 1 > 10:
                 if user.id not in self.queue:
                     self.queue.append(user.id)
-                    message += f'**#{place+1}**: {name} - added to queue'
+                    message += f"**#{place+1}**: {name} - added to queue"
             else:
-                message += f'**#{place+1}** : {name}\n'
+                message += f"**#{place+1}** : {name}\n"
 
         await ctx.send(message)
 
@@ -248,8 +263,7 @@ class Queue(commands.Cog):
 bot.add_cog(Queue(bot))
 
 # Place your token in a file called 'key' next to the script
-with open('key', 'r') as f:
+with open("key", "r") as f:
     token = f.read().strip().strip("\n")
 
 bot.run(token)
-
