1b9c6f40ed99d1eed81699869c4ec06820151a02
Chris Evans <chris.evans93@gmail.com>
2019-08-22 15:07:58 -0400

    Fixed stream sheets api stuff; still need to figure out API_KEY storage

--- InHouseBot/cogs/LeagueCog.py
+++ InHouseBot/cogs/LeagueCog.py
@@ -3,20 +3,20 @@ import httpx
 from discord.ext import commands
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-from gspread.utils import (
-    rowcol_to_a1,
-    fill_gaps,
-)
+from gspread.utils import rowcol_to_a1, fill_gaps
+
+SHEET_NUM_IDX = 1
+SHEET_NAME_IDX = 2
+SHEET_ID_IDX = 3
+SHEET_URL_IDX = 4
+
+
 class LeagueCog(commands.Cog):
     def __init__(self, bot, api_key=None):
 
+        self._init_sheet()
         self.client = httpx.AsyncClient()
-
-        # variables for gself.sheets
-        self.numindex = 1
-        self.nameindex = 2
-        self.idindex = 3
-        self.urlindex = 4
+        self.api_key = api_key
 
         # break is a keyword so we can't define it on class, interesting
         self.broke = False
@@ -33,60 +33,37 @@ class LeagueCog(commands.Cog):
         )
         self.clients = gspread.authorize(self.creds)
         self.sheet = self.clients.open("InHouseData").sheet1
-        data = self.sheet.get_all_records()
-
+        # data = self.sheet.get_all_records()
 
     @commands.command()
     async def addstream(self, ctx, url=""):
         """ Configure your stream to our database! """
         user = ctx.message.author
-        # values_list = self.sheet.get_all_values()
-        values_list = self.self.sheet.spreadself.sheet.values_get(self.self.sheet.title), {"key": self.api_key}
+        values_list = self.sheet.get_all_values()
 
-        try:
-            # Error for fill_gaps; Not sure what the alternative for 'values' can be
-            return fill_gaps(values_list["values"])
-        except KeyError:
-            return []
-
+        # If user exists, just update stream and exit
         for idx, element in enumerate(values_list):
             if element[2] == str(user.id):
-                # self.sheet.update_cell(idx+1, self.urlindex, url)
-                range_label = "%s!%s" % (
-                    self.sheet.title,
-                    rowcol_to_a1(idx + 1, self.urlindex),
-                )
-                data = self.sheet.spreadself.sheet.values_update(
-                    range_label,
-                    params={"valueInputOption": "USER_ENTERED", "key": self.api_key},
-                    body={"values": [[url]]},
-                )
-                return data
+                self.sheet.update_cell(idx + 1, SHEET_URL_IDX, url)
+                return
 
+        # Otherwise add an entire new row for the user
         userlist = [len(values_list), user.name, str(user.id), url]
-        # self.sheet.append_row(userlist)
-        params = {"valueInputOption": "RAW", "key": self.api_key}
-
-        body = {"values": [userlist]}
-        self.sheet.spreadself.sheet.values_append(self.sheet.title, params, body)
+        self.sheet.append_row(userlist)
 
     @commands.command()
     async def stream(self, ctx):
         """ Post your own stream """
         user = ctx.message.author
-        # values_list = self.sheet.get_all_values()
-        values_list = self.sheet.spreadself.sheet.values_get(self.sheet.title), {"key": self.api_key}
+        values_list = self.sheet.get_all_values()
+        for row in values_list:
+            # We are not using SHEET_* constants becuase this is a python array
+            # representation of the data, not indexing remotely
+            if row[2] == str(user.id):
+                msg = row[3]
+                return await ctx.send(msg)
 
-        try:
-            return fill_gaps(values_list["values"])
-        except KeyError:
-            return []
-
-        for _, element in enumerate(values_list):
-            if element[2] == str(user.id):
-                msg = element[3]
-                await ctx.send(f"{msg}")
-                return
+        # User not found
         await ctx.send(
             "You do not have any stream set up yet. Use !addstream to configure."
         )
@@ -94,20 +71,13 @@ class LeagueCog(commands.Cog):
     @commands.command()
     async def streams(self, ctx):
         """ Show list of streams """
-        # values_list = self.sheet.get_all_values()
-        values_list = self.sheet.spreadself.sheet.values_get(self.sheet.title), {"key": self.api_key}
+        values_list = self.sheet.get_all_values()
 
-        try:
-            return fill_gaps(values_list["values"])
-        except KeyError:
-            return []
+        max_name_len = max([len(x[1]) for x in values_list[1:]])
 
-        msg = ""
-        for idx, element in enumerate(values_list):
-            if idx == 0:
-                continue
-            else:
-                msg += f"**{element[1]}**: {element[3]}\n"
+        # We are not using SHEET_* constants becuase this is a python array
+        # representation of the data, not indexing remotely
+        msg = "".join(["**{0: <{pad}}**:\t{1}\n".format(row[1], row[3], pad=max_name_len) for row in values_list[1:]])
         await ctx.send(msg)
 
     @commands.command(
